f8b2a494e63c93a7982055314d4723f0e9d415e6
Dongsheng Cai <d@tux.im>
2018-01-03 15:06:57 +1100

    Enhance orgmode

--- org.el
+++ org.el
@@ -18,41 +18,44 @@
   (let ((daily-name (format-time-string org-journal-file-format)))
     (expand-file-name daily-name org-journal-dir)))
 
+;; custom vars
+(setq
+    local-org-data-directory "~/Dropbox/Documents/Org/"
+)
+
 ;; org-journal
 (setq
     org-journal-dir "~/Dropbox/Documents/Journal/"
     org-journal-file-format "%Y%m%d.org")
 ;; orgmode
 (setq
+    org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED"))
+    org-modules (append org-modules '(org-habit))
     org-startup-folded nil
     org-time-clocksum-format (quote (:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
     org-duration-format (quote h:mm)
     org-log-done t
     org-tag-alist '(("@work" . ?w) ("@home" . ?h))
-    org-directory "~/Dropbox/Documents/Org/"
-    org-default-notes-file (expand-file-name "refile.org" org-directory)
+    org-default-notes-file (expand-file-name "refile.org" local-org-data-directory)
+    org-agenda-files (list local-org-data-directory)
     org-default-journal-file 'get-journal-file-today
     org-clock-persist-file (expand-file-name "org-clock-save.el" emacs-local-dir)
     org-clock-persist 'history)
+
 (setq org-capture-templates
-      '(("t" "todo" entry (file org-default-notes-file)
-	 "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-	("r" "respond" entry (file org-default-notes-file)
-	 "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-	("n" "note" entry (file org-default-notes-file)
-	 "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-	;;("j" "Journal" entry (file+datetree org-default-journal-file)
-	;; "* %?\n%U\n" :clock-in t :clock-resume t)
-	("j" "Journal"
-	 entry (file (get-journal-file-today))
-	 "** Event: %?\n  %i"
-         :empty-lines 1)
-	("w" "org-protocol" entry (file org-default-notes-file)
-	 "* TODO Review %c\n%U\n" :immediate-finish t)
-	("m" "Meeting" entry (file org-default-notes-file)
-	 "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-	("h" "Habit" entry (file org-default-notes-file)
-	 "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
+  '(
+    ;;("j" "journal" entry (file (get-journal-file-today)) "* Event: %?\n  %i")
+    ("j" "journal" entry (file (get-journal-file-today)) "* %(format-time-string org-journal-time-format)%i%?")
+    ("t" "todo"    entry (file org-default-notes-file) "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+    ("r" "respond" entry (file org-default-notes-file) "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+    ("n" "note"    entry (file org-default-notes-file) "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+    ;;("j" "Journal" entry (file+datetree org-default-journal-file) "* %?\n%U\n" :clock-in t :clock-resume t)
+    ("m" "Meeting" entry (file org-default-notes-file) "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+    ("h" "Habit"   entry (file org-default-notes-file)
+	"* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")
+    ("w" "org-protocol" entry (file org-default-notes-file) "* TODO Review %c\n%U\n" :immediate-finish t)
+  )
+)
 ;; Standard key bindings
 (global-set-key "\C-cl" 'org-store-link)
 ;; org-agenda
@@ -62,5 +65,14 @@
 ;; I use C-c c to start capture mode
 (global-set-key (kbd "C-c c") 'org-capture)
 
-(add-hook 'find-file-hook 'auto-insert)
-(add-to-list 'auto-mode-alist `(,(expand-file-name org-journal-dir) . journal-file-insert))
+;; (add-hook 'find-file-hook 'auto-insert)
+;; (add-to-list 'auto-mode-alist `(,(expand-file-name org-journal-dir) . journal-file-insert))
+
+(defun air-pop-to-org-agenda (split)
+  "Visit the org agenda, in the current window or a SPLIT."
+  (interactive "P")
+  (org-agenda-list)
+  (when (not split)
+    (delete-other-windows)))
+
+(define-key global-map (kbd "C-c t a") 'air-pop-to-org-agenda)
