682d3cbd9c3a3fd38aa0748311ef554a88deb3d9
Dongsheng Cai <d@tux.im>
2018-01-04 09:50:56 +1100

    enable menu and toolbar in graphic mode

--- users/default/00-ui.el
+++ users/default/00-ui.el
@@ -1,4 +1,8 @@
-(menu-bar-mode -1) 
-;;(toggle-scroll-bar -1) 
-(tool-bar-mode -1) 
+(when (display-graphic-p)
+  (progn
+    (menu-bar-mode 1)
+    ;;(toggle-scroll-bar -1)
+    (tool-bar-mode 1)
+    )
+  )
 (load-theme 'gruvbox t)
