813981fffacf1aa18b6480fba8fe56176cb7db09
Dongsheng Cai <d@tux.im>
2018-01-04 21:07:03 +1100

    Better font for windows 10

--- users/default/ui.el
+++ users/default/ui.el
@@ -2,15 +2,19 @@
   (menu-bar-mode 1)
   (toggle-scroll-bar 1)
   (tool-bar-mode -1)
+  (set-face-attribute 'default nil :height 165)
+
+  (when (eq system-type 'darwin)
+    (set-face-attribute 'default nil :family "Monaco")
+    )
+
+  (when (eq system-type 'windows-nt)
+    (set-face-attribute 'default nil :family "Consolas")
+    )
   )
 
 (use-package gruvbox-theme
   :ensure t
   :config
   (load-theme 'gruvbox-dark-medium t)
-  )
-
-(when (eq system-type 'darwin)
-  (set-face-attribute 'default nil :family "Monaco")
-  (set-face-attribute 'default nil :height 165)
   )
