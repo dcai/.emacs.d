19515f9d0ca1ba6d961fdf10bad45fab19e2f099
Dongsheng Cai <d@tux.im>
2018-01-05 15:04:59 +1100

    evil: added a few plugins

--- evil.el
+++ evil.el
@@ -20,6 +20,23 @@
       "bd" 'kill-this-buffer
       )
     )
+  (use-package evil-nerd-commenter
+    :ensure t
+    :after evil-leader
+    :config
+    (global-evil-leader-mode)
+    (evil-leader/set-key
+      "ci" 'evilnc-comment-or-uncomment-lines
+      "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+      "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+      "cc" 'evilnc-copy-and-comment-lines
+      "cp" 'evilnc-comment-or-uncomment-paragraphs
+      "cr" 'comment-or-uncomment-region
+      "cv" 'evilnc-toggle-invert-comment-line-by-line
+      "."  'evilnc-copy-and-comment-operator
+      "\\" 'evilnc-comment-operator ; if you prefer backslash key
+      )
+    )
 
   (use-package let-alist
     :ensure t
@@ -34,10 +51,30 @@
           (evil-org-set-key-theme))))
     )
 
+  (use-package evil-matchit
+    :ensure t
+    :config
+    (global-evil-matchit-mode 1)
+    )
+
+  (use-package evil-visualstar
+    :ensure t
+    :config
+    (global-evil-visualstar-mode)
+    )
+
+  (use-package evil-matchit
+    :ensure t
+    :config
+    (global-evil-matchit-mode 1)
+    )
+
   (use-package evil-surround
     :ensure t
     :config
     (global-evil-surround-mode))
 
   (use-package evil-indent-textobject
-    :ensure t))
+    :ensure t
+    )
+  )
