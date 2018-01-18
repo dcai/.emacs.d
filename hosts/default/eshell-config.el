361b729115c00d92c0262889eed9e21feba411e1
Dongsheng Cai <d@tux.im>
2018-01-18 23:22:45 +1100

    better eshell

---
+++ hosts/default/eshell-config.el
@@ -0,0 +1,30 @@
+;; http://www.howardism.org/Technical/Emacs/eshell-fun.html
+(defun my-eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                   (file-name-directory (buffer-file-name))
+                   default-directory))
+          (height (/ (window-total-height) 3))
+          (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
+(global-set-key (kbd "C-`") 'my-eshell-here)
+
+(defalias 'vim 'find-file)
+(defalias 'vi 'find-file)
+(defalias 'edit 'find-file)
+(defalias 'emacs 'find-file)
