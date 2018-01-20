6174af22382fee8b1d872593ce0a36c5ffe504d1
Dongsheng Cai <d@tux.im>
2018-01-20 20:04:38 +1100

    Change smex save file path

--- package.el
+++ package.el
@@ -75,6 +75,9 @@
   (global-set-key (kbd "M-X") 'smex-major-mode-commands)
   ;; This is your old M-x.
   (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+  (setq
+    smex-save-file (expand-file-name "smex-items" my-emacs-data-dir)
+    )
   )
 
 (use-package editorconfig
