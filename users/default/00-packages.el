2c24df1426dfc064a6379de5e40fee395007cfec
Dongsheng Cai <d@tux.im>
2018-01-23 17:20:42 +1100

    hippie-expand for yasnippet+emmet

--- users/default/00-packages.el
+++ users/default/00-packages.el
@@ -22,19 +22,44 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;   Start to load other plugins
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package emmet-mode
+  :ensure t
+  :init
+  (add-hook 'css-mode-hook 'emmet-mode)
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  :config
+  (setq-default emmet-move-cursor-between-quote t)
+  (unbind-key "<C-return>" emmet-mode-keymap)
+  (unbind-key "C-M-<left>" emmet-mode-keymap)
+  (unbind-key "C-M-<right>" emmet-mode-keymap)
+  )
+
+(use-package hippie-exp
+  :ensure nil
+  :defer t
+  :bind ("<tab>" . hippie-expand)
+  :config
+  (setq-default hippie-expand-try-functions-list
+    '(yas-hippie-try-expand emmet-expand-line)))
+
 (use-package yasnippet
   :ensure t
+  :init
+  (add-hook 'js-mode-hook 'yas-minor-mode)
+  (add-hook 'sgml-mode-hook 'yas-minor-mode)
   :config
   (use-package yasnippet-snippets
     :ensure t
     )
+  (yas-reload-all)
+  (unbind-key "TAB" yas-minor-mode-map)
+  (unbind-key "<tab>" yas-minor-mode-map)
   (setq yas-snippet-dirs
     (append yas-snippet-dirs
       (list (expand-file-name "snippets" my-emacs-dotfiles-dir))
       )
     )
-  (yas-global-mode 1)
-  (add-hook 'prog-mode-hook #'yas-minor-mode)
+
   )
 
 ;; End of Install the missing packages
