fdc6736fe9abe754a10661dd8fb61346bc4b4659
Dongsheng Cai <d@tux.im>
2018-01-25 15:34:21 +1100

    magit: windows performance

--- users/default/00-packages.el
+++ users/default/00-packages.el
@@ -121,6 +121,19 @@
 
 (use-package magit
   :ensure t
+  :config
+  ;; WORKAROUND https://github.com/magit/magit/issues/2395
+  (define-derived-mode magit-staging-mode magit-status-mode "Magit staging"
+    "Mode for showing staged and unstaged changes."
+    :group 'magit-status)
+  (defun magit-staging-refresh-buffer ()
+    (magit-insert-section (status)
+      (magit-insert-untracked-files)
+      (magit-insert-unstaged-changes)
+      (magit-insert-staged-changes)))
+  (defun magit-staging ()
+    (interactive)
+    (magit-mode-setup #'magit-staging-mode))
   )
 
 (use-package avy
