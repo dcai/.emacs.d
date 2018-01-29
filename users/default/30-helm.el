5d3c9d5a5a9338588bccdd066c68c4ae36506710
Dongsheng Cai <d@tux.im>
2018-01-29 14:43:42 +1100

    Fix helm-ag search for rg

--- users/default/30-helm.el
+++ users/default/30-helm.el
@@ -41,7 +41,7 @@
     :config
     (custom-set-variables
       ;; '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
-      '(helm-ag-base-command "rg --no-heading --ignore-case")
+      '(helm-ag-base-command "rg --no-heading --vimgrep --ignore-case")
       '(helm-ag-command-option "--all-text")
       '(helm-ag-insert-at-point 'symbol)
       )
